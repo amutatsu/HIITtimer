18785c99d1446b9ed26a3e19ea1be1c19233b472
amutatsu <amutatsu007@gmail.com>
2019-11-21 23:52:50 +0900

    add timer

--- HIITtimer/ViewController.swift
+++ HIITtimer/ViewController.swift
@@ -1,12 +1,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var trainingTime: UILabel!
+    @IBOutlet weak var intervalTime: UILabel!
+    var trainingTotalTime = 20
+    var intervalTotalTime = 10
+    var tTimer = Timer()
+    var iTimer = Timer()
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        
+        tTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTrainingTimer), userInfo: nil, repeats: true)
+        
     }
-
-
+    
+    @IBAction func stopButtonPressed(_ sender: UIButton) {
+        
+        tTimer.invalidate()
+        iTimer.invalidate()
+        trainingTotalTime = 20
+        intervalTotalTime = 10
+    
+    }
+    
+    @objc func updateTrainingTimer() {
+        
+        if trainingTotalTime > 0{
+            trainingTotalTime -= 1
+            trainingTime.text = "\(trainingTotalTime) s"
+        } else {
+            tTimer.invalidate()
+            trainingTotalTime = 20
+            trainingTime.text = "\(trainingTotalTime) s"
+        }
+        
+        if trainingTotalTime == 0{
+            iTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateIntervalTimer), userInfo: nil, repeats: true)
+        }
+    
+    }
+    
+    @objc func updateIntervalTimer() {
+        
+        if intervalTotalTime > 0{
+            intervalTotalTime -= 1
+            intervalTime.text = "\(intervalTotalTime) s"
+        } else {
+            iTimer.invalidate()
+            intervalTotalTime = 10
+            intervalTime.text = "\(intervalTotalTime) s"
+        }
+        
+        if intervalTotalTime == 0{
+            tTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTrainingTimer), userInfo: nil, repeats: true)
+        }
+        
+    }
+    
 }
 
